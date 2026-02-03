2d71ae812ec74f5051b7099ce42ab8599843999e
yuna1412 <yuna1412@users.noreply.github.com>
2026-02-03 18:19:24 +0900

    環境構築中（laravel導入）

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
 
 function App() {
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src='/src/assets/react.svg' className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
