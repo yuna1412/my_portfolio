26f4abd22ca06fdc8718ad11ba49e9d9c481ae0d
yuna1412 <yuna1412@users.noreply.github.com>
2026-02-13 11:56:47 +0900

    top画面作成

--- frontend/src/pages/top/Page.tsx
+++ frontend/src/pages/top/Page.tsx
@@ -3,11 +3,9 @@ import "../../styles/pages/top.scss";
 
 export default function Top() { 
     return (
-        <section>
-            <div className="top">
-                <ContentSection title="Projects" />
-                <ContentSection title="Log" />
-            </div>
-        </section>
+        <div className="top">
+            <ContentSection title="Projects" />
+            <ContentSection title="Log" />
+        </div>
     );
 }
--- frontend/src/pages/top/parts/ContentSection.tsx
+++ frontend/src/pages/top/parts/ContentSection.tsx
@@ -4,15 +4,27 @@ type ContentSectionProps = {
   title: string;
 };
 
+const cards = [
+  { title: "タイトル", date: new Date(), detail: "This is the first card.This is the first card.This is the first card.This is the first card." },
+  { title: "タイトル", date: new Date(), detail: "This is the second card.This is the second card.This is the second card.This is the second card." },
+  { title: "タイトル", date: new Date(), detail: "This is the third card.This is the third card.This is the third card.This is the third card.This is the third card." },
+  { title: "タイトル", date: new Date(), detail: "This is the fourth card.This is the fourth card.This is the fourth card.This is the fourth card.This is the fourth card." },
+  { title: "タイトル", date: new Date(), detail: "This is the fifth card." },
+];
+
 export default function ContentSection({ title }: ContentSectionProps) {
     return (
         <section className="contentSection">
             <h2 className="contentSection__title">{title}</h2>
             <div className="contentSectionWrapper">
-                <Card title="Card" date={new Date(2023, 5, 15)} detail="This is the first card." />
-                <Card title="Card2" date={new Date(2023, 5, 16)} detail="This is the second card." />
-                <Card title="Card3" date={new Date(2023, 5, 17)} detail="This is the third card." />
-                <Card title="Card4" date={new Date(2023, 5, 18)} detail="This is the fourth card." />
+                {cards.slice(0, 4).map((card, index) => (
+                    <Card
+                      key={index}
+                      title={card.title}
+                      date={card.date}
+                      detail={card.detail}
+                    />
+                  ))}
             </div>
         </section>
     );
