02a5485ec18115cba0a5b40bccd17f6f5ef5817e
yuna1412 <yuna1412@users.noreply.github.com>
2026-02-13 18:51:19 +0900

    コメントアウト削除

--- frontend/src/pages/log/Page.tsx
+++ frontend/src/pages/log/Page.tsx
@@ -31,36 +31,3 @@ export default function Log() {
     );
 }
 
-// import { useNavigate } from "react-router-dom";
-
-// import Card from "../../components/ui/Card";
-// import { projects } from "../../data/projects";
-
-// import "../../styles/pages/projects.scss";
-
-
-// export default function Projects() { 
-//     const navigate = useNavigate();
-
-
-//     return (
-//         <div className="projects">
-//             <section className="projectsSection">
-//                 <h2 className="projectsSection__title">Projects</h2>
-//                 <div className="projectsSectionWrapper">
-//                     {projects.map((project) => (
-//                       <Card
-//                         key={project.id}
-//                         id={project.id}
-//                         image={project.image}
-//                         title={project.title}
-//                         date={project.date}
-//                         detail={project.detail}
-//                         onClick={(id) => navigate(`/projects/${id}`)}
-//                       />
-//                     ))}
-//                 </div>
-//             </section>
-//         </div>
-//     );
-// }
