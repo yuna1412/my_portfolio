546dc25595b006f797e880bb57a0a00bf01c8ad6
yuna1412 <yuna1412@users.noreply.github.com>
2026-02-13 15:05:47 +0900

    profile画面作成

--- frontend/src/pages/profile/Page.tsx
+++ frontend/src/pages/profile/Page.tsx
@@ -3,7 +3,26 @@ import "../../styles/pages/profile.scss";
 export default function Profile() { 
     return (
         <div className="profile">
-            <h1 className="title">Profile</h1>
+            <section className="profileSection">
+                <h2 className="profileSection__title">Profile</h2>
+                <div className="profileSectionWrapper">
+                    <div className="profileInfo">
+                        <img src="/images/sample02.jpg" alt="Sample Profile Image" className="profileInfo__image" />
+                        <div className="profileInfo__texts">
+                            <p className="profileInfo__name">名前：tesuto</p>
+                            <p className="profileInfo__text">
+                                主にフロントエンド開発を中心に携わってきました。<br/>
+                                ワイヤーフレーム作成やコーディング等、フロントエンド領域の開発が得意です。<br/>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="profileDetails">
+                        <p>2022年　インターンにてWeb制作を行う。</p>
+                        <p>2024年　エンジニアとしてキャリアを開始。現在に至る。</p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
+
